e3508b5ca73a5655d68dc1212f59c17f8f2c7e9a
htun7520 <sanghyun123452@gmail.com>
2026-01-08 11:37:16 +0900

    ADD: 사용자 로그인 프론트 로직

--- backend/routing_system/src/main/java/com/smart/complaint/routing_system/applicant/controller/ApplicantController.java
+++ backend/routing_system/src/main/java/com/smart/complaint/routing_system/applicant/controller/ApplicantController.java
@@ -6,6 +6,8 @@ import com.smart.complaint.routing_system.applicant.dto.NormalizationResponse;
 import com.smart.complaint.routing_system.applicant.service.AiService;
 import com.smart.complaint.routing_system.applicant.service.ApplicantService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.attribute.UserPrincipal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,24 +29,19 @@ public class ApplicantController {
     private final AiService aiService;
     private final ApplicantService applicantService;
 
-    @GetMapping("/api/home")
-    public ResponseEntity<?> login(@AuthenticationPrincipal OAuth2User principal) {
-        if(principal == null) {
-            return null;
+    // 토큰 유효성 검사 엔드포인트
+    @GetMapping("/api/auth/validate")
+    public ResponseEntity<?> validateToken(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        // JwtAuthenticationFilter를 거쳐 여기까지 왔다면 토큰은 유효한 것입니다.
+        if (userPrincipal != null) {
+            return ResponseEntity.ok().build(); // 200 OK
         }
-
-        // 로그인한 사용자의 정보를 담아서 전송
-        Map<String, Object> userInfo = new HashMap<>();
-        userInfo.put("id", principal.getAttribute("id"));
-        userInfo.put("name", principal.getName());
-
-        // TODO: 확인용, 이후 변경할 것
-        return ResponseEntity.ok(userInfo);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 401
     }
 
     @PostMapping("/api/complaints")
     public ResponseEntity<NormalizationResponse> sendComplaints(@AuthenticationPrincipal String applicantId,
-                                                                @RequestBody ComplaintDto request) {
+            @RequestBody ComplaintDto request) {
 
         NormalizationResponse aiData = aiService.getNormalization(request);
 
@@ -61,7 +59,7 @@ public class ApplicantController {
 
     @GetMapping("/api/complaints")
     public ResponseEntity<List<ComplaintDto>> getAllComplaints(@AuthenticationPrincipal String applicantId) {
-        
+
         // 현재 로그인한 사용자의 모든 민원 조회
         List<ComplaintDto> complaints = applicantService.getAllComplaints(applicantId);
 
