f469f49b85ce550cbc7a6fbf659ba24f8587447c
hyun7520 <sanghyun123452@gmail.com>
2026-01-21 07:47:53 +0900

    ADD: 주소 수정

--- ai-server/main.py
+++ ai-server/main.py
@@ -104,7 +104,7 @@ async def preprocess_complaint(req: ComplaintRequest, request: Request):
         api_key = os.getenv("LANGFLOW_KEY")
         # url = "http://complaint-langflow:7860/api/v1/run/59369f82-0d62-414e-bd20-9bc5f9aa8a50"  # The complete API endpoint URL for this flow
         # 서버 전용 langflow api url
-        url = "http://34.158.210.224:7860/api/v1/run/fef3bc76-0d0f-45c7-b91a-e6c017b2b1e8"
+        url = "http://34.158.210.224:7860/api/v1/run/38dc29fa-ae0b-40dd-a3d2-cac77b1cb5ab"
 
         for i in req:
             print(i)
@@ -122,10 +122,10 @@ async def preprocess_complaint(req: ComplaintRequest, request: Request):
                 #     "input_value": req.body
                 # }
                 # 서버 전용
-                "TITLE-QaZnz": {
+                "TITLE-7NcS7": {
                     "input_value": req.title
                 },
-                "BODY-d62U4": {
+                "BODY-JCktr": {
                     "input_value": req.body
                 }
             }
